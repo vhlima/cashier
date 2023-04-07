918294ceaf0a849c82f12da2110116753caaf165
vhlima <vhlima2000@gmail.com>
2023-04-07 18:34:52 -0300

    📱 Improve responsivity on MovieHeader

--- src/views/MenuCategory/components/MenuHeader/index.tsx
+++ src/views/MenuCategory/components/MenuHeader/index.tsx
@@ -4,7 +4,7 @@ import { SearchBar } from '../index';
 
 export const MenuHeader: React.FC = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col justify-between gap-4 md:flex-row">
       <Typography component="h1" size="3xl">
         <strong>Menu</strong>
         &nbsp;Category
