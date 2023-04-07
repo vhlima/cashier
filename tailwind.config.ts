4dc349f0db02ff3ce3b8e0cb77f8dbe5a4c33f1e
vhlima <vhlima2000@gmail.com>
2023-04-07 13:29:01 -0300

    🔧 Add color palette to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
-import { type Config } from "tailwindcss";
+import { type Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#f6f6f6',
+        grey: '#b5c0ca',
+        yellow: '#f1dbad',
+        brown: '#524239',
+        red: '#dc2120',
+        orange: '#dfa345',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
