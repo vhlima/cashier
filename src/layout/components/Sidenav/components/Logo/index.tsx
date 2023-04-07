5be62fc5db8f2f2498c343ac84160258f6d82673
vhlima <vhlima2000@gmail.com>
2023-04-07 18:21:55 -0300

    💄 Change logo spacing

--- src/layout/components/Sidenav/components/Logo/index.tsx
+++ src/layout/components/Sidenav/components/Logo/index.tsx
@@ -1,6 +1,6 @@
 export const Logo: React.FC = () => {
   return (
-    <div className="flex items-center justify-center border border-red p-4">
+    <div className="my-8 flex items-center justify-center border-red px-4 py-8">
       <span>M</span>
     </div>
   );
