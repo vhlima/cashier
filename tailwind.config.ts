d44a3e34907d6bf3808210aed08e2d1e8a8c7449
vhlima <vhlima2000@gmail.com>
2023-04-07 16:32:45 -0300

    🔧 Add roboto font to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import { type Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
     extend: {
       colors: {
         'white-low': '#f6f6f6',
