ac920d49699fb095affcc37d9d3c193034cf75df
vhlima <vhlima2000@gmail.com>
2023-04-08 10:41:36 -0300

    💄 Add margin to MenuCategories

--- src/views/MenuCategory/components/MenuCategories/index.tsx
+++ src/views/MenuCategory/components/MenuCategories/index.tsx
@@ -10,7 +10,7 @@ export const MenuCategories: React.FC<Props> = props => {
   const { selectedCategory } = props;
 
   return (
-    <nav className="flex gap-8" data-testid="menu-categories">
+    <nav className="my-12 flex gap-8" data-testid="menu-categories">
       {menuCategories.map(category => (
         <MenuCategory
           key={`menu-category-${category.name}`}
