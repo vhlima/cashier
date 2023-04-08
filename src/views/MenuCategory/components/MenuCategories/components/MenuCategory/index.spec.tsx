6d8f205e4887b2941aec10e67ef550592bf51413
vhlima <vhlima2000@gmail.com>
2023-04-08 10:19:36 -0300

    ✅ Ensure menu category icon is being rendered correctly

---
+++ src/views/MenuCategory/components/MenuCategories/components/MenuCategory/index.spec.tsx
@@ -0,0 +1,38 @@
+import { type RenderResult, render } from '@testing-library/react';
+
+import { AiFillAccountBook } from 'react-icons/ai';
+
+import { faker } from '@faker-js/faker';
+
+import { MenuCategory } from '.';
+
+type SutType = {
+  sut: RenderResult;
+  name: string;
+  route: string;
+};
+
+function createSut(): SutType {
+  const name = faker.word.adjective();
+  const route = `/${faker.word.adjective()}`;
+
+  const sut = render(
+    <MenuCategory name={name} route={route} icon={AiFillAccountBook} />,
+  );
+
+  return {
+    sut,
+    name,
+    route,
+  };
+}
+
+describe('MenuCategory', () => {
+  test('Should render icon correctly', () => {
+    const { sut } = createSut();
+
+    const iconElement = sut.getByTestId('menu-category-icon');
+    expect(iconElement).toBeInTheDocument();
+    expect(iconElement.closest('svg')).not.toBeNull();
+  });
+});
