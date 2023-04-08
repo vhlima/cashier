67c21a30898de8f506d36e5a205d279ce9026d76
vhlima <vhlima2000@gmail.com>
2023-04-08 13:51:55 -0300

    🐛 Fix Product not being rendered on server side

--- src/views/MenuCategory/components/MenuProducts/components/Product/index.tsx
+++ src/views/MenuCategory/components/MenuProducts/components/Product/index.tsx
@@ -1,4 +1,6 @@
-import { motion } from 'framer-motion';
+import { motion, type HTMLMotionProps } from 'framer-motion';
+
+import { isBrowser } from '@/utils';
 
 import type { Product } from '../../products';
 
@@ -12,14 +14,20 @@ interface Props extends Product {
 export const MenuProduct: React.FC<Props> = props => {
   const { displayOrder, name, price, displayImageUrl, isNew } = props;
 
+  const animationProps: HTMLMotionProps<'li'> = isBrowser()
+    ? {
+        initial: { opacity: 0, y: -50 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: 50 },
+        transition: { duration: 0.5, delay: displayOrder * 0.05 },
+      }
+    : {};
+
   return (
     <motion.li
       className="relative flex flex-col items-center"
-      initial={{ opacity: 0, y: -50 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }}
-      transition={{ duration: 0.5, delay: displayOrder * 0.01 }}
       data-testid="menu-product"
+      {...animationProps}
     >
       {isNew && <NewProductTag />}
 
