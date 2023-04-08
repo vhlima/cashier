43352693079e73f29f3a9be6d2b1491ee109ce4d
vhlima <vhlima2000@gmail.com>
2023-04-08 10:30:14 -0300

    💄 Add testid to MenuCategories

--- src/views/MenuCategory/components/MenuCategories/index.tsx
+++ src/views/MenuCategory/components/MenuCategories/index.tsx
@@ -10,7 +10,7 @@ export const MenuCategories: React.FC<Props> = props => {
   const { selectedCategory } = props;
 
   return (
-    <nav className="flex gap-8">
+    <nav className="flex gap-8" data-testid="menu-categories">
       {menuCategories.map(category => (
         <MenuCategory
           key={`menu-category-${category.name}`}
