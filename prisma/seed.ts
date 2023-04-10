fbc9c7a98bc691511e9603234ef87c57828846db
vhlima <vhlima2000@gmail.com>
2023-04-10 14:33:37 -0300

    ✨ Implement existing seed files with prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+
+import { createBurgers } from './seeds';
+
+const client = new PrismaClient();
+
+async function main() {
+  console.log('[Seed] Starting seeding.');
+
+  await createBurgers(client);
+
+  console.log('[Seed] Seeding completed.');
+}
+
+main()
+  .then(async () => {
+    await client.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await client.$disconnect();
+    process.exit(1);
+  });
