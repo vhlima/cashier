a3153b712e3a951e5ea8caa218fe55bb3169e21a
vhlima <vhlima2000@gmail.com>
2023-04-11 13:30:19 -0300

    🎨 Add product router

---
+++ src/server/api/routers/product.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+
+export const productRouter = createTRPCRouter({
+  getProductTypeByName: publicProcedure
+    .input(z.object({ productTypeName: z.string() }))
+    .query(({ input, ctx }) => {
+      const { productTypeName } = input;
+
+      return ctx.prisma.productType.findUniqueOrThrow({
+        where: {
+          name: productTypeName,
+        },
+      });
+    }),
+  getAllByTypeId: publicProcedure
+    .input(z.object({ productTypeId: z.string().nullable() }))
+    .query(({ input, ctx }) => {
+      const { productTypeId } = input;
+
+      if (!productTypeId) {
+        return ctx.prisma.product.findMany({
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      }
+
+      return ctx.prisma.product.findMany({
+        where: {
+          productTypeId,
+        },
+      });
+    }),
+});
