4aa4f8b471e8ec2e9715dc11ae37d148c4fa89bf
vhlima <vhlima2000@gmail.com>
2023-04-11 13:29:59 -0300

    ✨ Add nextjs compability with trpc

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,22 +4,49 @@
  *
  * We also create a few inference helpers for input and output types.
  */
-import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCNext } from "@trpc/next";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import superjson from "superjson";
+import { httpBatchLink, loggerLink } from '@trpc/client';
+import { createTRPCNext } from '@trpc/next';
+import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
+import superjson from 'superjson';
 
-import { type AppRouter } from "@/server/api/root";
+import { type AppRouter } from '@/server/api/root';
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (typeof window !== 'undefined') return ''; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
+    if (typeof window !== undefined) {
+      return {
+        /**
+         * Transformer used for data de-serialization from the server.
+         *
+         * @see https://trpc.io/docs/data-transformers
+         */
+        transformer: superjson,
+
+        /**
+         * Links used to determine request flow from client to server.
+         *
+         * @see https://trpc.io/docs/links
+         */
+        links: [
+          loggerLink({
+            enabled: opts =>
+              process.env.NODE_ENV === 'development' ||
+              (opts.direction === 'down' && opts.result instanceof Error),
+          }),
+          httpBatchLink({
+            url: `${getBaseUrl()}/api/trpc`,
+          }),
+        ],
+      };
+    }
+
     return {
       /**
        * Transformer used for data de-serialization from the server.
@@ -35,14 +62,27 @@ export const api = createTRPCNext<AppRouter>({
        */
       links: [
         loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
+          enabled: opts =>
+            process.env.NODE_ENV === 'development' ||
+            (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
+      headers() {
+        if (!ctx?.req?.headers) {
+          return {};
+        }
+        // To use SSR properly, you need to forward the client's headers to the server
+        // This is so you can pass through things like cookies when we're server-side rendering
+        const {
+          // If you're using Node 18 before 18.15.0, omit the "connection" header
+          connection: _connection,
+          ...headers
+        } = ctx.req.headers;
+        return headers;
+      },
     };
   },
   /**
@@ -50,7 +90,7 @@ export const api = createTRPCNext<AppRouter>({
    *
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: false,
+  ssr: true,
 });
 
 /**
