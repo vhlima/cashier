5fde42848a8860467e95dfc0a03babe340835e49
vhlima <vhlima2000@gmail.com>
2023-05-04 23:59:09 -0300

    💄 Add different sizes for ProductImage

--- src/components/Product/components/ProductImage/index.tsx
+++ src/components/Product/components/ProductImage/index.tsx
@@ -1,16 +1,24 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 interface Props {
   src: string;
   alt: string;
+  size?: 'sm' | 'md' | 'lg';
 }
 
 export const ProductImage: React.FC<Props> = props => {
-  const { src, alt } = props;
+  const { src, alt, size = 'md' } = props;
 
   return (
-    <div className="rounded-sm bg-yellow-100 p-4">
-      <div className="relative h-20 w-20">
+    <div className="flex items-center justify-center rounded-sm bg-yellow-100 p-4">
+      <div
+        className={clsx('relative', {
+          'h-20 w-20': size === 'sm',
+          'h-24 w-24': size === 'md',
+          'h-64 w-64': size === 'lg',
+        })}
+      >
         <Image
           className="relative z-10 object-cover"
           src={src}
