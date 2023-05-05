2191e01d6b3e471c0c58f393c700706c5f1548b0
vhlima <vhlima2000@gmail.com>
2023-05-04 23:28:36 -0300

    🎨 Add useProduct hook

---
+++ src/components/Product/hooks/useProduct.tsx
@@ -0,0 +1,48 @@
+import {
+  type PropsWithChildren,
+  createContext,
+  useMemo,
+  useContext,
+} from 'react';
+
+type ProductData = {
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+};
+
+interface ProductContextData {
+  product: ProductData;
+}
+
+const ProductContext = createContext({} as ProductContextData);
+
+export const ProductContextProvider: React.FC<
+  PropsWithChildren<ProductContextData>
+> = props => {
+  const { product, children } = props;
+
+  const contextValue = useMemo(
+    (): ProductContextData => ({
+      product,
+    }),
+    [product],
+  );
+
+  return (
+    <ProductContext.Provider value={contextValue}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export function useProduct(): ProductContextData {
+  const context = useContext(ProductContext);
+
+  if (!context) {
+    throw new Error('useProduct must be used within an provider');
+  }
+
+  return context;
+}
