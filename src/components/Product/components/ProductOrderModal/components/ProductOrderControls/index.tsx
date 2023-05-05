8c6c6f40a0a6de10281a3febe58ea8741f77a441
vhlima <vhlima2000@gmail.com>
2023-05-05 19:58:32 -0300

    🔥 Remove ProductOrderContextProvider from ProductOrderControls

--- src/components/Product/components/ProductOrderModal/components/ProductOrderControls/index.tsx
+++ src/components/Product/components/ProductOrderModal/components/ProductOrderControls/index.tsx
@@ -1,4 +1,4 @@
-import { ProductOrderContextProvider, useQuantitySelector } from '../../hooks';
+import { useQuantitySelector } from '../../hooks';
 
 import { AmountSelector, AddToCartButton } from '../index';
 
@@ -12,18 +12,16 @@ export const ProductOrderControls: React.FC<Props> = props => {
   const quantitySelector = useQuantitySelector(1);
 
   return (
-    <ProductOrderContextProvider>
-      <div className="mt-4 grid grid-rows-2 gap-4 lg:grid-cols-2">
-        <AmountSelector
-          className="w-full border border-gray-200"
-          {...quantitySelector}
-        />
+    <div className="mt-4 grid grid-rows-2 gap-4 lg:grid-cols-2">
+      <AmountSelector
+        className="w-full border border-gray-200"
+        {...quantitySelector}
+      />
 
-        <AddToCartButton
-          productPrice={productPrice}
-          quantity={quantitySelector.quantity}
-        />
-      </div>
-    </ProductOrderContextProvider>
+      <AddToCartButton
+        productPrice={productPrice}
+        quantity={quantitySelector.quantity}
+      />
+    </div>
   );
 };
