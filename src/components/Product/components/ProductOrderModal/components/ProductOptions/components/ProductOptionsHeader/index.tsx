62f5678fba6e2b37e1bface82109cfccb50f6e08
vhlima <vhlima2000@gmail.com>
2023-05-06 20:36:47 -0300

    🎨 Add className and optional props to ProductOptionsHeader

--- src/components/Product/components/ProductOrderModal/components/ProductOptions/components/ProductOptionsHeader/index.tsx
+++ src/components/Product/components/ProductOrderModal/components/ProductOptions/components/ProductOptionsHeader/index.tsx
@@ -1,15 +1,22 @@
+import { type HTMLAttributes } from 'react';
+
+import clsx from 'clsx';
+
 import { Typography } from '@/components';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   title: string;
   description: string;
 }
 
 export const ProductOptionsHeader: React.FC<Props> = props => {
-  const { title, description } = props;
+  const { className, title, description, ...rest } = props;
 
   return (
-    <div className="bg-slate-100 px-4 py-2">
+    <div
+      className={clsx('bg-slate-100 px-4 py-2', className && className)}
+      {...rest}
+    >
       <Typography className="font-bold" component="h2">
         {title}
       </Typography>
