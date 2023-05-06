bd5989a4e656ee4347db138a9d1e01c300032312
vhlima <vhlima2000@gmail.com>
2023-05-05 23:02:22 -0300

    🔧 Disable eslint rule causing incompability with prisma typings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ const config = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
   },
 };
 
