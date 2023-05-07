ea2f8346dd9f12d1c9e046fd13a4e5d21a2e8690
vhlima <vhlima2000@gmail.com>
2023-05-07 00:44:01 -0300

    🎨 Add forwardRef to Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { type PropsWithChildren, type HTMLAttributes } from 'react';
+import { type PropsWithChildren, type HTMLAttributes, forwardRef } from 'react';
 
 import { createPortal } from 'react-dom';
 
@@ -10,40 +10,44 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 const PORTAL_ID = 'modals';
 
-export const Modal: React.FC<PropsWithChildren<Props>> = props => {
-  const { className, center, onClickBackdrop, children, ...rest } = props;
+/* eslint-disable react/display-name */
+export const Modal = forwardRef<HTMLDivElement, PropsWithChildren<Props>>(
+  (props, ref) => {
+    const { className, center, onClickBackdrop, children, ...rest } = props;
 
-  const modalBody = (
-    <div
-      className={clsx(
-        {
-          'absolute h-full w-full lg:left-1/2 lg:top-1/4 lg:h-fit lg:max-w-screen-lg lg:-translate-x-1/2 lg:transform':
-            center,
-        },
-        className && className,
-      )}
-      role="presentation"
-      onClick={e => e.stopPropagation()}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
+    const modalBody = (
+      <div
+        className={clsx(
+          {
+            'absolute h-full w-full lg:left-1/2 lg:top-1/4 lg:h-fit lg:max-w-screen-lg lg:-translate-x-1/2 lg:transform':
+              center,
+          },
+          className && className,
+        )}
+        role="presentation"
+        onClick={e => e.stopPropagation()}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
 
-  const modalContent = !!onClickBackdrop ? (
-    <div
-      className="absolute z-40 h-screen w-screen overflow-hidden bg-black bg-opacity-50"
-      role="presentation"
-      onClick={onClickBackdrop}
-    >
-      {modalBody}
-    </div>
-  ) : (
-    modalBody
-  );
+    const modalContent = !!onClickBackdrop ? (
+      <div
+        className="absolute z-40 h-screen w-screen overflow-hidden bg-black bg-opacity-50"
+        role="presentation"
+        onClick={onClickBackdrop}
+      >
+        {modalBody}
+      </div>
+    ) : (
+      modalBody
+    );
 
-  return createPortal(
-    modalContent,
-    document.getElementById(PORTAL_ID) || document.body,
-  );
-};
+    return createPortal(
+      modalContent,
+      document.getElementById(PORTAL_ID) || document.body,
+    );
+  },
+);
