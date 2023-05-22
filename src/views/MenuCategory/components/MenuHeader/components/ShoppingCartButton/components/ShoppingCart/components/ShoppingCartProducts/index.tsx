cef476b2ed6de46f9e8228521c2ef737e2ad68b1
vhlima <vhlima2000@gmail.com>
2023-05-22 12:33:42 -0300

    💄 Change conditional rendering priority on ShoppingCartProducts

--- src/views/MenuCategory/components/MenuHeader/components/ShoppingCartButton/components/ShoppingCart/components/ShoppingCartProducts/index.tsx
+++ src/views/MenuCategory/components/MenuHeader/components/ShoppingCartButton/components/ShoppingCart/components/ShoppingCartProducts/index.tsx
@@ -12,30 +12,34 @@ export const ShoppingCartProducts: React.FC = () => {
       productsIds: products.map(product => product.productId),
     });
 
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!productsData) {
+    return <h1>No products, click here to add.</h1>;
+  }
+
   return (
     <ProductOrderModalContextProvider>
       <ul>
-        {!productsData || isLoading ? (
-          <h1>loading...</h1>
-        ) : (
-          products.map(productInfo => {
-            const productFound = productsData.find(
-              currentProduct => currentProduct.id === productInfo.productId,
-            );
+        {products.map(productInfo => {
+          const productFound = productsData.find(
+            currentProduct => currentProduct.id === productInfo.productId,
+          );
 
-            if (!productFound) {
-              return null;
-            }
+          if (!productFound) {
+            return null;
+          }
 
-            return (
-              <ShoppingCartProduct
-                key={`shopping-cart-product-${productFound.id}`}
-                quantity={productInfo.quantity}
-                {...productFound}
-              />
-            );
-          })
-        )}
+          return (
+            <ShoppingCartProduct
+              key={`shopping-cart-product-${productFound.id}`}
+              quantity={productInfo.quantity}
+              {...productFound}
+            />
+          );
+        })}
       </ul>
     </ProductOrderModalContextProvider>
   );
