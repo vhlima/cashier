4de632dde056bb37830239a435eb6e3523760097
vhlima <vhlima2000@gmail.com>
2023-05-22 14:07:21 -0300

    💄 Change ShoppingCartButton amount background color

--- src/layout/components/Navbar/components/ShoppingCartButton/index.tsx
+++ src/layout/components/Navbar/components/ShoppingCartButton/index.tsx
@@ -3,7 +3,7 @@ import { FaShoppingCart } from 'react-icons/fa';
 export const ShoppingCartButton: React.FC = () => {
   return (
     <button className="relative p-2" type="button">
-      <span className="absolute right-0 top-0 h-4 w-4 rounded-full bg-red text-xs text-white">
+      <span className="absolute right-0 top-0 h-4 w-4 rounded-full bg-primary text-xs text-white">
         {0}
       </span>
 
