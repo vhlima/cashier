c7c93265c23cab05cc6928235e077872b39d0aca
vhlima <vhlima2000@gmail.com>
2023-05-23 08:50:19 -0300

    💄 Remove margin from SearchBar

--- src/layout/components/Navbar/components/SearchBar/index.tsx
+++ src/layout/components/Navbar/components/SearchBar/index.tsx
@@ -2,7 +2,7 @@ import { FaSearch } from 'react-icons/fa';
 
 export const SearchBar: React.FC = () => {
   return (
-    <div className="mt-2 flex items-center rounded-md border border-quaternary bg-tertiary">
+    <div className="flex items-center rounded-md border border-quaternary bg-tertiary">
       <div className="px-4">
         <FaSearch className="text-primary" size={20} />
       </div>
