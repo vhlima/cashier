7e583a94ec6e1f349a71576170fb4c4ec53296fa
vhlima <vhlima2000@gmail.com>
2023-05-23 09:03:55 -0300

    ✨ Implement ShoppingCartButton

--- src/layout/components/Navbar/components/ShoppingCartButton/index.tsx
+++ src/layout/components/Navbar/components/ShoppingCartButton/index.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
+
 import { FaShoppingCart } from 'react-icons/fa';
 
-export const ShoppingCartButton: React.FC = () => {
-  return (
-    <button className="relative p-2" type="button">
-      <span className="absolute right-0 top-0 h-4 w-4 rounded-full bg-primary text-xs text-white">
-        {0}
-      </span>
+import { useShoppingCart } from '@/hooks';
 
-      <FaShoppingCart className="text-t-tertiary" size={20} />
-    </button>
+import { ShoppingCart } from '@/views/MenuCategory/components/MenuHeader/components/ShoppingCartButton/components';
+
+export const ShoppingCartButton: React.FC = () => {
+  const { products } = useShoppingCart();
+
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      {isModalOpen && <ShoppingCart onClose={() => setModalOpen(false)} />}
+
+      <button
+        className="relative p-2"
+        type="button"
+        onClick={() => setModalOpen(true)}
+        data-testid="shopping-cart-button"
+      >
+        <span
+          className="absolute right-0 top-0 h-4 w-4 rounded-full bg-primary text-xs text-white"
+          data-testid="shopping-cart-quantity"
+        >
+          {products.length}
+        </span>
+
+        <FaShoppingCart
+          className="text-t-tertiary"
+          size={20}
+          data-testid="shopping-cart-icon"
+        />
+      </button>
+    </>
   );
 };
